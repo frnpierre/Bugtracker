4f7b29b2184cc185a73b0a835093977ad8889dff
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-01-12 15:32:17 +0000

    Add project creation functionality & system test for it

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,21 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+  
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:success] = "You created the project: #{@project.name}"
+      redirect_to root_url
+    else 
+      flash.now[:error] = "There was something wrong."
+      render :new
+    end
+  end
+  
+  private
+  
+    def project_params
+      params.require(:project).permit(:name).merge(user: current_user)
+    end
 end
--- spec/system/project_creation_spec.rb
+++ spec/system/project_creation_spec.rb
@@ -2,4 +2,23 @@ require "rails_helper"
 
 RSpec.describe "Project creation", type: :system do
 
+  let(:user) { User.create(email: "test@example.com", password: "password") }
+  
+  it "by a logged in user is possible" do
+    projects_count = Project.count
+    sign_in(user)
+    visit new_project_path
+    fill_in("project_name", with: "Project test name")
+    click_on("Create")
+    expect(page).to have_selector(".alert-success")
+    expect(Project.count).to eq(projects_count + 1)
+  end
+  
+  it "by a logged out user is NOT possible" do
+    sign_in(user)
+    sign_out(user)
+    visit new_project_path
+    expect(current_path).to eq(new_user_session_path)
+  end
+  
 end
