dac745331a831057b008ab00c1ac1277604b5f17
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-01-12 14:07:23 +0000

    Fix test description for StaticPagesController

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe StaticPagesController, type: :controller do
 
   describe "GET #home" do
-    it "returns http success" do
+    it "returns http redirect" do
       get :home
       expect(response).to have_http_status(:redirect)
     end
