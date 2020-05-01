7fe98b65d2a9fe1e1cc2f931e0a57e30563310b5
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-05-01 18:41:09 +0000

    Add project_ownership helper

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,11 @@
 module ProjectsHelper
+  
+  def project_ownership(project)
+    if project.user_id == current_user.id
+      "You created this project."
+    else 
+      "#{project.user.username} created this project.
+      You are part of this project team."
+    end
+  end
 end
