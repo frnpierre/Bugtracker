6769495225c19d748ba59556799258d0a32364ea
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-05-01 19:19:42 +0000

    UI: Authorization on project#show (Add user/remove buttons)

--- spec/system/team_memberships/team_membership_creation_spec.rb
+++ spec/system/team_memberships/team_membership_creation_spec.rb
@@ -8,10 +8,15 @@ RSpec.describe "TeamMembership creation", type: :system do
   let!(:project) { create(:project, name: "Project membership",
                                    description: "Test project to test membership",
                                    user_id: user.id ) }
+  let!(:project_with_team) { create(:project, description: "Project owned by user_two",
+                                              user_id: user_two.id,
+                                              allowed_users: [user]) }
+  
   
   it "A logged in a user can add another user to his project team" do
     sign_in(user)
     visit project_path(project)
+    expect(page).to have_link("Add user", href: new_project_team_membership_path(project))
     expect(current_path).to eq(project_path(project))
     expect(page).not_to have_text(user_two.username)
     click_on("Add user")
@@ -20,5 +25,11 @@ RSpec.describe "TeamMembership creation", type: :system do
     click_on("Add")
     expect(current_path).to eq(project_path(project))
     expect(page).to have_text(user_two.username)
+  end
+  
+  it "A logged in user can't add another user to a project he's in the team" do
+    sign_in(user)
+    visit project_path(project_with_team)
+    expect(page).not_to have_link("Add user", href: new_project_team_membership_path(project_with_team))
   end
 end
--- spec/system/team_memberships/team_membership_destroy_spec.rb
+++ spec/system/team_memberships/team_membership_destroy_spec.rb
@@ -5,19 +5,35 @@ RSpec.describe "TeamMembership destroy", type: :system do
   let(:user) { create(:user) } 
   let(:user_two) { create(:user, username: "team_member",
                                  email: "team_member@example.com") }
+  let(:user_three) { create(:user, username: "team_member can't remove") }
+  
   let!(:project) { create(:project, name: "Project membership",
-                                   description: "Test project to test membership",
-                                   user_id: user.id ) }
+                                    description: "Test project to test membership",
+                                    user_id: user.id ) }
+                                    
   let!(:team_membership) { TeamMembership.create(user_id: user_two.id, project_id: project.id) } 
+  
+  # project created by user two, with team: user, user_three 
+  let!(:project_with_team) {  create(:project, description: "project owned by user_two",
+                                               user_id: user_two.id,
+                                               allowed_users: [user, user_three] ) }
   
   it "A logged in a user can remove another user from his project team" do
     sign_in(user)
     visit project_path(project)
     expect(current_path).to eq(project_path(project))
     expect(page).to have_text(user_two.email)
+    expect(page).to have_link("remove", href: project_team_membership_path(project, team_membership))
     click_on("remove")
     expect { team_membership.reload }.to raise_error ActiveRecord::RecordNotFound
     expect(current_path).to eq(project_path(project))
     expect(page).not_to have_text(user_two.email)
+  end
+  
+  it "A logged in user can't remove another user to a project he's in the team" do
+    sign_in(user)
+    visit project_path(project_with_team)
+    membership = TeamMembership.find_by(user_id: user_three.id, project_id: project_with_team.id)
+    expect(page).not_to have_link("remove", href: project_team_membership_path(project_with_team, membership))
   end
 end
