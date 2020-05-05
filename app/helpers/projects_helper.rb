872fc943eac7278c396c478731b47ec3a0b79860
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-05-05 01:44:41 +0000

    Improve project_ownership helper & add dependent: :destroy for project team memberships

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,54 @@
 module ProjectsHelper
   
   def project_ownership(project)
+    # if current user owns the project
     if project.user_id == current_user.id
-      "You created this project."
+      if current_page?(projects_path)
+        @data = "<p class='project-owner'>You created this project.</p>" 
+      else
+        index_link = link_to("the project index page", projects_path)
+        
+        @data = "<div class='project-ownership'>
+                  <p>You created this project.</p>
+                  
+                  <span>You can:</span>
+                  <ul>
+                    <li>Manage the team</li>
+                    <li>Report bugs</li>
+                    <li>Mark as solved / edit / delete all the bugs</li>
+                    <li>Comment on bugs</li>
+                    <li>Edit or delete the project via #{index_link}</li>
+                  </ul>
+                </div>"
+      end
+      @data.html_safe
+    # if current user doesn't own the project
     else 
-      "#{project.user.username} created this project.
-      You are part of this project team."
+      if current_page?(projects_path)
+        @data = "<p class='project-owner'>
+                   #{project.user.username} created this project.
+                   You are part of this project team.
+                 </p>" 
+      else
+        @data = "<div class='project-ownership'>
+                  <p>
+                    #{project.user.username} created this project.
+                    You are part of this project team.
+                  </p>
+                  
+                  <span>You can:</span>
+                  <ul>
+                    <li>Report bugs</li>
+                    <li>Mark as solved / edit / delete your own bugs</li>
+                    <li>Comment on all bugs</li>
+                  </ul>
+                  <p>
+                    If you wish to manage the team or all the bugs of the project,
+                    you need to log in as #{project.user.username}
+                  </p>
+                </div>"
+      end
+      @data.html_safe
     end
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ApplicationRecord
   belongs_to :user
   has_many :bugs, dependent: :destroy
   
-  has_many :team_memberships
+  has_many :team_memberships, dependent: :destroy
   has_many :allowed_users, through: :team_memberships, source: :user
 end
