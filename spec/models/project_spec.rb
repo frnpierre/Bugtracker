270ef70a591e594bbe27d7f77148c81eef3f6c6b
frnpierre <59374864+frnpierre@users.noreply.github.com>
2020-05-16 22:23:18 +0000

    Improve Project validations unit tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -3,18 +3,24 @@ require 'rails_helper'
 RSpec.describe Project, type: :model do
   
   let(:user) { create(:user) }
-  let(:project) { user.projects.build }
+  let(:project_to_validate) { user.projects.build }
   
   context "Validations:" do 
     
     it "name should be present" do 
-      expect(project.valid?).to be_falsy
-      expect(project.errors.messages[:name]).not_to be_empty
+      expect(project_to_validate.valid?).to be_falsy
+      expect(project_to_validate.errors.messages[:name]).not_to be_empty
+      project_to_validate.name = "Valid name"
+      project_to_validate.valid?
+      expect(project_to_validate.errors.messages[:name]).to be_empty
     end
     
     it "description should be present" do 
-      expect(project.valid?).to be_falsy
-      expect(project.errors.messages[:description]).not_to be_empty
+      expect(project_to_validate.valid?).to be_falsy
+      expect(project_to_validate.errors.messages[:description]).not_to be_empty
+      project_to_validate.description = "Valid description"
+      project_to_validate.valid?
+      expect(project_to_validate.errors.messages[:description]).to be_empty
     end
   end
 end
